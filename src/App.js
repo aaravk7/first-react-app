634b87f2d595cd8ac1a8275a92312fdeb03099f1
Aarav <aaravkhokhar@yahoo.in>
2022-01-09 15:34:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <div className="container mt-5">
+        <div className="row mt-5">
+          <Card title="Tesla" desc="Best electric car in the world." imgUrl="https://tesla-cdn.thron.com/delivery/public/image/tesla/da705069-91b5-41cb-86f3-86a585c6fdf3/bvlatuR/std/2880x1800/MX-Hero-Desktop" />
+          <Card title="Honda" desc="dskafndlkjsanfkljndskanfkdsnaflknsda" imgUrl="https://imgd.aeplcdn.com/664x374/cw/360/honda/1100/closed-door/15.jpg?q=65" />
+          <Card title="Audi" desc="One of the most premium cars." imgUrl="https://imgd.aeplcdn.com/0x0/n/cw/ec/48342/rs7-sportback-exterior-right-front-three-quarter-3.jpeg" />
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = (props) => {
+    return (
+        <div className="card col-4">
+            <img src={props.imgUrl} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">{props.title}</h5>
+                <p className="card-text">{props.desc}</p>
+                <a href="#" className="btn btn-danger">Go somewhere</a>
+            </div>
+        </div>
+    )
+}
+
+export default Card
